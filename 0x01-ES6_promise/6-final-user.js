9ee883f8f3177c89639ba34a01205d900bf80518
berekettena <tena21bereket@gmail.com>
2024-03-06 20:04:02 +0300

    last check fixed

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,7 +2,7 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.callAll([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((disp) => (
       disp.map((beki) => ({
         status: beki.status,
